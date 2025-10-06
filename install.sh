5e7bd446ea0e5e2a68c1948ff4fdedbad51ac044
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2025-10-06 13:06:38 -0300

    Unify install scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-# Update and upgrade the system packages
-sudo apt update
-sudo apt upgrade -y
+# Update system
+apt update && apt -y upgrade
+
+# install necessary packages
+apt install -y samba-ad-dc krb5-user bind9-dnsutils
+
+# disable unnecessary services
+systemctl disable --now smbd nmbd winbind
+systemctl mask smbd nmbd winbind
+
+# enable and unmask samba-ad-dc service
+systemctl unmask samba-ad-dc
+systemctl enable samba-ad-dc
+
+# backup original smb.conf
+mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
+
+# provision the samba AD DC
+samba-tool domain provision --use-rfc2307 --interactive
+
+# set samba as the DNS backend
+domain=$(samba-tool domain info 127.0.0.1 | grep 'Domain' | awk '{print $3}')
+unlink /etc/resolv.conf
+echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+echo "search $domain" >> /etc/resolv.conf
+
+# disable systemd-resolved to avoid conflicts
+systemctl disable --now systemd-resolved
+
+# krb5.conf setup
+cp -f /var/lib/samba/private/krb5.conf /etc/krb5.conf
+
+# start the samba-ad-dc service
+systemctl start samba-ad-dc
